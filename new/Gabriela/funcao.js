8caf52c2f6c8bc11cec19e4f0692744691ca4a16
Allison Joanine <122819284+AllisonJoanine@users.noreply.github.com>
2023-10-16 15:14:48 -0300

    Aulas

---
+++ new/Gabriela/funcao.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    // Define um temporizador para executar uma função após um atraso de 1000 milissegundos (1 segundo)
+
+    document.querySelector('.box').classList.add('fade');
+    // Seleciona o elemento com a classe "box" usando document.querySelector e adiciona a classe "fade" a esse elemento
+
+}, 1000); // O atraso é definido como 1000 milissegundos (1 segundo)
